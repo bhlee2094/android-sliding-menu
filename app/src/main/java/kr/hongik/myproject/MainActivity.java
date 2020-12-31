3d55eec80d07221439676e08bcd4c4811af2db4a
bhlee <djbh123@naver.com>
2020-12-31 19:28:11 +0900

    사진 찍어 이미지뷰에 띄우기 구현 (수정)

--- app/src/main/java/kr/hongik/myproject/MainActivity.java
+++ app/src/main/java/kr/hongik/myproject/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -24,6 +29,13 @@ import com.pedro.library.AutoPermissions;
 import com.pedro.library.AutoPermissionsListener;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
     boolean isPageOpen = false;
@@ -32,9 +44,13 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     Animation translateRightAnim;
 
     LinearLayout page;
-    Button menubutton, photobutton;
+    Button menubutton;
     ImageView imageView;
-    File file;
+
+    static File currentPhotoFile;
+    static Uri currentPhotoUri;
+    static String currentPhotoPath;
+    static String currentPhotoFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,14 +79,6 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
             }
         });
 
-        photobutton = findViewById(R.id.photoButton); //사진찍기 버튼
-        photobutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePickture();
-            }
-        });
-
         AutoPermissions.Companion.loadAllPermissions(this, 101); //자동권한 부여
     }
 
@@ -94,38 +102,165 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
         public void onAnimationRepeat(Animation animation) { }
     }
 
-    public void takePickture() { //이 메서드 호출시 파일 만듬
-        if(file == null){
-            file = createFile();
-        }
+    public void takePickture(View view) throws IOException { //이 메서드 호출시 파일 만듬
 
-        Uri fileUri = FileProvider.getUriForFile(this, "kr.hongik.myproject.fileprovider", file); //Uri 객체
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //카메라 앱 띄우기
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-        if(intent.resolveActivity(getPackageManager()) !=null) {
-            startActivityForResult(intent, 101);
+        if(intent.resolveActivity(getPackageManager()) !=null) { //이미지파일생성
+            File imageFile = createImageFile();
+            if(imageFile != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
-    private File createFile() { //사진을 찍은 후 저장할 파일
-        String filename = "capture.jpg";
-        File storageDir = Environment.getExternalStorageDirectory();
-        File outFile = new File(storageDir, filename);
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
 
-        return outFile;
+        File imagePath = getExternalFilesDir("images"); //외부저장소 사용
+        File newFile = File.createTempFile(imageFileName, ".jpg", imagePath); //파일생성
+
+        currentPhotoFile = newFile;
+        currentPhotoFileName = newFile.getName();
+        currentPhotoPath = newFile.getAbsolutePath(); //파일의 절대경로를 저장한다.
+
+        try {
+            currentPhotoUri = FileProvider.getUriForFile(this,
+                    getApplicationContext().getPackageName() + ".fileprovider",
+                    newFile);
+        } catch (Exception ex) {
+            Log.d("FileProvider", ex.getMessage());
+            ex.printStackTrace();
+            throw ex;
+        }
+
+        return newFile;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { //카메라 앱 닫기
         super.onActivityResult(requestCode,resultCode,data);
 
-        if(requestCode ==101 && resultCode == RESULT_OK) {
-            BitmapFactory.Options options = new BitmapFactory.Options(); //이미지 파일을 bitmap 객체로 만들기
-            options.inSampleSize = 8;
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+        if(requestCode ==REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if(currentPhotoUri != null) {
+                imageView.setImageURI(currentPhotoUri);
 
-            imageView.setImageBitmap(bitmap); //이미지뷰에 bitmap 설정하기
+                galleryAddPic(currentPhotoUri, currentPhotoFileName);
+            }
         }
+        if(requestCode ==REQUEST_IMAGE_OPEN && resultCode ==RESULT_OK){
+            Uri selectImageUri = data.getData();
+
+            Bitmap antiRotationBitmap = createAntiRotationSampleBitmap(selectImageUri, imageView.getWidth(), imageView.getHeight());
+            imageView.setImageBitmap(antiRotationBitmap);
+        }
+    }
+
+    private Uri galleryAddPic(Uri srcImageFileUri, String srcImageFileName) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, srcImageFileName);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/MyImages");
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 1);
+        Uri newImageFileUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        try{
+            AssetFileDescriptor afdInput = contentResolver.openAssetFileDescriptor(srcImageFileUri, "r");
+            AssetFileDescriptor afdOutput = contentResolver.openAssetFileDescriptor(newImageFileUri, "w");
+            FileInputStream fis = afdInput.createInputStream();
+            FileOutputStream fos = afdOutput.createOutputStream();
+
+            byte[] readByteBuf = new byte[1024];
+            while(true){
+                int readLen = fis.read(readByteBuf);
+                if(readLen <= 0){
+                    break;
+                }
+                fos.write(readByteBuf, 0, readLen);
+            }
+
+            fos.flush();
+            fos.close();
+            afdOutput.close();
+
+            fis.close();
+            afdInput.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        contentValues.clear();
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0);
+        contentResolver.update(newImageFileUri, contentValues, null, null);
+        return newImageFileUri;
+    }
+
+    private Bitmap createAntiRotationSampleBitmap(Uri scrImageFileUri, int dstWidth, int dstHeight) {
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        ParcelFileDescriptor pfdExif = null;
+        int orientation = 0;
+        try {
+            pfdExif = contentResolver.openFileDescriptor(scrImageFileUri, "r");
+            FileDescriptor fdExif = pfdExif.getFileDescriptor();
+            ExifInterface exifInterface = null;
+            exifInterface = new ExifInterface(fdExif);
+            orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+            Log.d("orientation", String.valueOf(orientation));
+            pfdExif.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = createSampledBitmap(scrImageFileUri, dstWidth, dstHeight);
+
+        Matrix matrix = new Matrix();
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1); //좌우반전
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+//              matrix.setRotate(180);
+//              matrix.postScale(-1, 1); //좌우반전
+                matrix.setScale(1, -1); //상하반전
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1); //좌우반전
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1); //좌우반전
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap antiRotationBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle(); //bitmap은 더이상 필요 없음으로 메모리에서 free시킨다.
+            return antiRotationBitmap;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @Override
